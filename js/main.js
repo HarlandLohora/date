314bdad589e647a7afbaf37e406adb133ad6d8c2
Harland <lohoraharland@gmail.com>
2018-05-13 07:14:56 -0500

    methods

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+
+
+
+
 function date(){
-	alert("DATE");
+	
+	setInterval(function(){
+		var d = new Date();
+		document.getElementById("date").innerHTML = d;
+		console.log(d.getFullYear());
+		console.log(d.getMonth());
+		console.log(d.getDate());
+		console.log(d.getHours());
+		console.log(d.getMinutes());
+		console.log(d.getSeconds());
+
+	},1000);
 }
+
+date();
